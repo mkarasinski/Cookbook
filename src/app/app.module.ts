73b157e43997dda2340df472858ca5f5d2d9fd70
mkarasinski <maciej.karasinski.95@gmail.com>
2020-07-07 12:04:22 +0200

    Authorization added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -16,6 +17,12 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { RecipeService } from './recipes/recipe.service';
+import { AuthComponent } from './auth/auth.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
+import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 @NgModule({
   declarations: [
@@ -30,9 +37,27 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     DropdownDirective,
     RecipeStartComponent,
     RecipeEditComponent,
+    AuthComponent,
+    LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective,
   ],
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule, AppRoutingModule],
-  providers: [ShoppingListService],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,21 +9,28 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe('Solnik', 'Ulubiona potrawa świąteczna', 'assets/woda.jpg', [
-      new Ingredient('Woda', 1),
-      new Ingredient('Troche soli', 1),
-    ]),
-    new Recipe('Mielony', 'Najlepszy kotlecior', 'assets/mielony.jpg', [
-      new Ingredient('Mięso', 1),
-      new Ingredient('Cebula', 1),
-      new Ingredient('Bulka tarta', 1),
-      new Ingredient('Jajko', 1),
-      new Ingredient('Przyprawy do miesa', 1),
-    ]),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe('Solnik', 'Ulubiona potrawa świąteczna', 'assets/woda.jpg', [
+  //     new Ingredient('Woda', 1),
+  //     new Ingredient('Troche soli', 1),
+  //   ]),
+  //   new Recipe('Mielony', 'Najlepszy kotlecior', 'assets/mielony.jpg', [
+  //     new Ingredient('Mięso', 1),
+  //     new Ingredient('Cebula', 1),
+  //     new Ingredient('Bulka tarta', 1),
+  //     new Ingredient('Jajko', 1),
+  //     new Ingredient('Przyprawy do miesa', 1),
+  //   ]),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
